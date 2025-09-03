5aa3aa6adbf7bf72130a29c88ab2011b735363e9
Lokesh <23ca1031@mvn.edu.in>
2025-09-03 21:10:38 +0530

    Update command suffixes to include '1'

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -5,12 +5,12 @@ from bot import CMD_SUFFIX, config_dict
 class _BotCommands:
     def __init__(self):
         self.StartCommand = "start"
-        self.MirrorCommand = [f"mirror{CMD_SUFFIX}", f"m{CMD_SUFFIX}"]
-        self.QbMirrorCommand = [f"qbmirror{CMD_SUFFIX}", f"qm{CMD_SUFFIX}"]
-        self.YtdlCommand = [f"ytdl{CMD_SUFFIX}", f"y{CMD_SUFFIX}"]
-        self.LeechCommand = [f"leech{CMD_SUFFIX}", f"l{CMD_SUFFIX}"]
-        self.QbLeechCommand = [f"qbleech{CMD_SUFFIX}", f"ql{CMD_SUFFIX}"]
-        self.YtdlLeechCommand = [f"ytdlleech{CMD_SUFFIX}", f"yl{CMD_SUFFIX}"]
+        self.MirrorCommand = [f"mirror1{CMD_SUFFIX}", f"m1{CMD_SUFFIX}"]
+        self.QbMirrorCommand = [f"qbmirror1{CMD_SUFFIX}", f"qm1{CMD_SUFFIX}"]
+        self.YtdlCommand = [f"ytdl1{CMD_SUFFIX}", f"y1{CMD_SUFFIX}"]
+        self.LeechCommand = [f"leech1{CMD_SUFFIX}", f"l1{CMD_SUFFIX}"]
+        self.QbLeechCommand = [f"qbleech1{CMD_SUFFIX}", f"ql1{CMD_SUFFIX}"]
+        self.YtdlLeechCommand = [f"ytdlleech1{CMD_SUFFIX}", f"yl1{CMD_SUFFIX}"]
         if config_dict["SHOW_EXTRA_CMDS"]:
             self.MirrorCommand.extend(
                 [
@@ -48,14 +48,14 @@ class _BotCommands:
             self.YtdlLeechCommand.extend(
                 [f"ytdlzipleech{CMD_SUFFIX}", f"yzl{CMD_SUFFIX}"]
             )
-        self.CloneCommand = [f"clone{CMD_SUFFIX}", f"c{CMD_SUFFIX}"]
-        self.CountCommand = f"count{CMD_SUFFIX}"
-        self.DeleteCommand = f"del{CMD_SUFFIX}"
+        self.CloneCommand = [f"clone1{CMD_SUFFIX}", f"c1{CMD_SUFFIX}"]
+        self.CountCommand = f"count1{CMD_SUFFIX}"
+        self.DeleteCommand = f"del1{CMD_SUFFIX}"
         self.CancelMirror = f"cancel{CMD_SUFFIX}"
         self.CancelAllCommand = [f"cancelall{CMD_SUFFIX}", "cancellallbot"]
         self.ListCommand = f"list{CMD_SUFFIX}"
-        self.SearchCommand = f"search{CMD_SUFFIX}"
-        self.StatusCommand = [f"status{CMD_SUFFIX}", f"s{CMD_SUFFIX}", "statusall"]
+        self.SearchCommand = f"search1{CMD_SUFFIX}"
+        self.StatusCommand = [f"status1{CMD_SUFFIX}", f"s{CMD_SUFFIX}", "statusall"]
         self.UsersCommand = f"users{CMD_SUFFIX}"
         self.AuthorizeCommand = [f"authorize{CMD_SUFFIX}", f"a{CMD_SUFFIX}"]
         self.UnAuthorizeCommand = [f"unauthorize{CMD_SUFFIX}", f"ua{CMD_SUFFIX}"]
@@ -64,16 +64,16 @@ class _BotCommands:
         self.AddSudoCommand = f"addsudo{CMD_SUFFIX}"
         self.RmSudoCommand = f"rmsudo{CMD_SUFFIX}"
         self.PingCommand = [f"ping{CMD_SUFFIX}", f"p{CMD_SUFFIX}"]
-        self.RestartCommand = [f"restart{CMD_SUFFIX}", f"r{CMD_SUFFIX}", "restartall"]
-        self.StatsCommand = [f"stats{CMD_SUFFIX}", f"st{CMD_SUFFIX}"]
-        self.HelpCommand = f"help{CMD_SUFFIX}"
+        self.RestartCommand = [f"restart1{CMD_SUFFIX}", f"r{CMD_SUFFIX}", "restartall"]
+        self.StatsCommand = [f"stats1{CMD_SUFFIX}", f"st1{CMD_SUFFIX}"]
+        self.HelpCommand = f"help1{CMD_SUFFIX}"
         self.LogCommand = f"log{CMD_SUFFIX}"
         self.ShellCommand = f"shell{CMD_SUFFIX}"
         self.EvalCommand = f"eval{CMD_SUFFIX}"
         self.ExecCommand = f"exec{CMD_SUFFIX}"
         self.ClearLocalsCommand = f"clearlocals{CMD_SUFFIX}"
-        self.BotSetCommand = [f"bsetting{CMD_SUFFIX}", f"bs{CMD_SUFFIX}"]
-        self.UserSetCommand = [f"usetting{CMD_SUFFIX}", f"us{CMD_SUFFIX}"]
+        self.BotSetCommand = [f"bsetting1{CMD_SUFFIX}", f"bs1{CMD_SUFFIX}"]
+        self.UserSetCommand = [f"usetting1{CMD_SUFFIX}", f"us1{CMD_SUFFIX}"]
         self.BtSelectCommand = f"btsel{CMD_SUFFIX}"
         self.CategorySelect = f"ctsel{CMD_SUFFIX}"
         self.SpeedCommand = [f"speedtest{CMD_SUFFIX}", f"sp{CMD_SUFFIX}"]

437366a606565a43c20bdae9cc4aaed9c48be03b
Lokesh <23ca1031@mvn.edu.in>
2025-09-14 11:29:16 +0530

    Change progress bar characters in status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -194,8 +194,8 @@ def get_progress_bar_string(pct):
     pct = float(str(pct).strip("%"))
     p = min(max(pct, 0), 100)
     cFull = int(p // 8)
-    p_str = "⬢" * cFull
-    p_str += "⬡" * (12 - cFull)
+    p_str = "▰" * cFull
+    p_str += "▱" * (12 - cFull)
     return f"[{p_str}]"
 
 
